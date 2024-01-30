227d64d8a446ba5e5cce14343cc0679060f88e3c
Atrey <atrey@MacBook-Pro-Atrey.local>
2024-01-30 12:12:16 +0100

    Updates

---
+++ precache-manifest.28f2b248ccb3b39c6758a7b5e050c4c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc2f1b68fb0b61132f65f0fb0d46c68",
+    "url": "/cart-react/index.html"
+  },
+  {
+    "revision": "a84a7a4ec9bd3cad9f6c",
+    "url": "/cart-react/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "340329c76c4953fab3ba",
+    "url": "/cart-react/static/css/main.3f87028e.chunk.css"
+  },
+  {
+    "revision": "a84a7a4ec9bd3cad9f6c",
+    "url": "/cart-react/static/js/2.50b1e691.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/cart-react/static/js/2.50b1e691.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "340329c76c4953fab3ba",
+    "url": "/cart-react/static/js/main.e47e17e9.chunk.js"
+  },
+  {
+    "revision": "b960e5fddbff525f7170",
+    "url": "/cart-react/static/js/runtime-main.181fbc1f.js"
+  }
+]);
